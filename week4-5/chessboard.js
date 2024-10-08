042e73282c6df6bf83820a7d6c0bbab8c31dfab4
bdavis0321 <bryce.davis0321@gmail.com>
2024-10-08 10:01:18 -0400

    Update chessboard.js

--- week4-5/chessboard.js
+++ week4-5/chessboard.js
@@ -16,21 +16,3 @@ if (size > 0) {
     else {
         window.alert("Not a valid number.");
     }
-
-
-
-////////storage
-
-
-let h = prompt("Enter height.")
-let w = prompt("Enter width.")
-
-const hashodd = ' #';
-const hasheven = '# ';
-
-const hash = ' #';
-
-{
-    console.log(hashodd.repeat(size/2));
-    console.log(hasheven.repeat(size/2));
-}
