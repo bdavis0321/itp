de7ab7de8ed2cd9e025ad62f465b2676a24b9809
bdavis0321 <bryce.davis0321@gmail.com>
2024-10-08 09:46:31 -0400

    Update chessboard.js

---
+++ week4-5/chessboard.js
@@ -0,0 +1,24 @@
+const hashodd = ' #';
+const hasheven = '# ';
+
+let size = prompt("Enter size.")
+
+if (size > 0) {
+    for (let s = 1; s <= size; s++) {
+        console.log(hashodd.repeat(size/2));
+        console.log(hasheven.repeat(size/2));
+    }
+}
+
+
+
+////////storage
+
+
+let h = prompt("Enter height.")
+let w = prompt("Enter width.")
+
+const hashodd = ' #';
+const hasheven = '# ';
+
+const hash = ' #';
